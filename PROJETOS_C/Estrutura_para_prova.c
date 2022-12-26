bbcfd3dc7da7504a1427d6e55d4343384b833b27
Fabio-Oliveira-Dev <fabiooliveiraideia2@gmail.com>
2022-12-26 16:46:06 -0300

    Aulas Do While

---
+++ PROJETOS_C/Estrutura_para_prova.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int; // variaveis
+    //comando for/while/do while (inicializador, condição, incremento)
+    {
+        printf(" \n");
+        scanf("",);
+        if () // Condição
+        {
+            printf("");
+        }
+    }
+    return 0;
+}

31302f98abe3a88910f4b675261458ef86f8f582
Fabio-Oliveira-Dev <fabiooliveiraideia2@gmail.com>
2022-12-27 18:34:30 -0300

    Aula vetor

---
+++ PROJETOS_C/Vetor.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+
+// Acesso aos elementos Vator
+int vet[10];
+1 vet [0] = 5;
+2 vet [1] = 6;
+3 vet [3] = 8;
+4 vet [4] = 12;
+5 vet [5] = 18;
+6 vet [6] = 22;
+7 vet [7] = 34;
+8 vet [8] = 78;
+9 vet [9] = 45;
+
+int numeros[5] = {10,12,14,16,18}; //Inicialização durante a declaração
+
+printf("%d Posicao 1 do vetor:", vet[1]); // Comando para exibir um elemento do vetor
+
+scanf ("%d", &vet[7]); // Ler um dado do dispositivo de entrada e armazenar em posição
+
+vet[4]=90 // Atribuir valor a um elemento e substituir
+
+}
